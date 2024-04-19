aa0670ddac4238715ce324c016b5e64523dc0a6a
Deepak Patil <deepakpatil0999@gmail.com>
2024-04-19 10:20:34 +0530

    bug fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
   const pageName = pathname?.split("/")?.filter(Boolean)?.[0];
 
   return (
-    <header className="sticky top-0 z-20 flex flex-row items-center justify-between h-14 px-4 md:px-5 bg-white dark:bg-black">
+    <header className="sticky top-0 z-20 flex flex-row items-center justify-between h-14 px-4 md:px-5 bg-black">
       {/* loder animation is added  */}
       {loading && <Loader />}
       {/* Logo added */}
